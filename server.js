f1b3113c76b68ca32ebe9efa208bb50637316548
Ignas Mešinski <ignas.mesinski@yahoo.lt>
2025-08-31 13:49:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+
 import usersRoutes from "./routes/users.js";
 import itemsRoutes from "./routes/items.js";
 import tradesRoutes from "./routes/trades.js";
-const cors = require("cors");
-const authRoutes = require("./routes/authRoutes");
+import authRoutes from "./routes/authRoutes.js"; // mūsų JWT auth
 
 dotenv.config();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 // ROUTES
 app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
-app.use("/items", itemRoutes);
-app.use("/trades", tradeRoutes);
+app.use("/users", usersRoutes);
+app.use("/items", itemsRoutes);
+app.use("/trades", tradesRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
